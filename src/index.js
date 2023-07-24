7fef602e3f751190715e1ea00635036fbb612488
saisashreek <saisashreekd@gmail.com>
2023-07-24 10:55:20 +0530

    Custom Scrollbar Added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import App from "./App";
+import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
 // contexts
