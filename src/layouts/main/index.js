0b142364dde7c0531ca49ae3476b4f140f72fdb6
saisashreek <saisashreekd@gmail.com>
2023-08-08 15:42:38 +0530

    Settings Page UI Completed

--- src/layouts/main/index.js
+++ src/layouts/main/index.js
@@ -1,12 +1,21 @@
+import { Container, Stack } from "@mui/material";
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Logo from "../../assets/Images/logo.ico";
 
 const MainLayout = () => {
   return (
     <>
-      <div>Main Layout</div>
+      <Container maxWidth="sm" sx={{ mt: 5 }}>
+        <Stack spacing={5}>
+          <Stack sx={{ width: "100%" }} direction="column" alignItems="center">
+            <img style={{ width: 120, height: 120 }} src={Logo} alt="Logo" />
+          </Stack>
+        </Stack>
+        {/* <div>Main Layout</div> */}
 
-      <Outlet />
+        <Outlet />
+      </Container>
     </>
   );
 };
